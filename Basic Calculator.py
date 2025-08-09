d8e11a4970e010743ccfcb87c2d3dc9278f7cc06
Jusper-Ageri <ombokajusper@gmail.com>
2025-08-09 12:39:50 +0300

    Add files via upload

---
+++ Basic Calculator.py
@@ -0,0 +1,17 @@
+print("Enter the First Number: ")
+num1 = int(input())
+print("Enter the Second Number: ")
+num2 = int(input())
+
+Answer = num1+num2
+print("\nThe sum of the two numbers is =", Answer)
+
+Answer = num1-num2
+print("\nThe difference of the two numbers is =", Answer)
+
+Answer = num1*num2
+print("\nThe multiplication of the two numbers is =", Answer)
+
+Answer = num1/num2
+print("The division of the two numbers is =", Answer)
+
